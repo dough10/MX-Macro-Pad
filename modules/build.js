12ee9a2c0698a378b8a37d247a739355ad686672
dough10 <doughten.jimi@gmail.com>
2021-04-25 13:14:27 -0500

    U: colors, V: 1.0.5

--- modules/build.js
+++ modules/build.js
@@ -1,7 +1,8 @@
-var fs = require( 'fs' );
-var uglifyJS = require('uglify-es');
-var uglifycss = require('uglifycss');
-var minify = require('html-minifier').minify;
+const fs = require( 'fs' );
+const uglifyJS = require('uglify-es');
+const uglifycss = require('uglifycss');
+const minify = require('html-minifier').minify;
+const colors = require('colors');
 
 
 const files = [
@@ -16,27 +17,27 @@ const files = [
 
 
 function copyFile(filePath) {
-  console.log(`moving ${filePath} to HTML folder`);
+  console.log(`moving ${filePath.green} to HTML folder`);
   fs.createReadStream(`./src/${filePath}`).pipe(fs.createWriteStream(`./html/${filePath}`));
 }
 
 
 function minifyHTML() {
   return new Promise((resolve, reject) => {
-    console.log('Concatinating/Minifying HTML');
+    console.log('Concatinating/Minifying HTML'.yellow);
     fs.readFile('./src/index.html', 'utf8', (err, html) => {
       if (err) {
         reject(err);
         return;
       }
-      var smallHTML = minify(html, {
+      const smallHTML = minify(html, {
         removeAttributeQuotes: true,
         collapseWhitespace: true,
         minifyCSS: true,
         minifyURLs: true,
         removeComments: true
       });
-      console.log('/html/index.html has been saved');
+      console.log('/html/index.html'.green, 'has been saved');
       fs.writeFile( './html/index.html', smallHTML, resolve);
     });
   });
@@ -45,18 +46,18 @@ function minifyHTML() {
 
 function uglifyJavaScript() {
   return new Promise((resolve, reject) => {
-    console.log('Concatinating/Minifying Javascript');
+    console.log('Concatinating/Minifying Javascript'.yellow);
     fs.readFile('./src/js/app.js', 'utf8', (err, js) => {
       if (err) {
         reject(err);
         return;
       }
-      var uglyCode = uglifyJS.minify(js);
+      const uglyCode = uglifyJS.minify(js);
       if (uglyCode.error) {
         console.log(uglyCode.error);
         return;
       }
-      console.log('/html/js/app.js has been saved');
+      console.log('/html/js/app.js'.green, 'has been saved');
       fs.writeFile( './html/js/app.js', uglyCode.code, resolve);
     });
   });
@@ -65,17 +66,17 @@ function uglifyJavaScript() {
 
 function uglyCSS() {
   return new Promise(resolve => {
-    console.log('Concatinating/Minifying Stylesheets');
-    var uglified = uglifycss.processFiles(
+    console.log('Concatinating/Minifying Stylesheets'.yellow);
+    const uglified = uglifycss.processFiles(
         [
           './src/css/base.css'
         ],
         {
           maxLineLen: 500,
-          expandVars: true
+          expandconsts: true
         }
     );
-    console.log('/html/css/base.css has been saved');
+    console.log('/html/css/base.css'.green, 'has been saved');
     fs.writeFile( './html/css/base.css', uglified, resolve);
   });
 }
