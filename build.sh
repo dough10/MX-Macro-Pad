eab44de2b7f14b7a17e8978ee39ea579f3fcc777
dough10 <doughten.jimi@gmail.com>
2021-04-25 10:17:30 -0500

    U: colors!, V: 1.0.5

--- build.sh
+++ build.sh
@@ -1,32 +1,48 @@
 #!/bin/sh
+
+CODE='\033['
+
+black$CODE'0;30m'
+RED=$CODE'0;31m'
+GREEN=$CODE'0;32m'
+ORANGE=$CODE'0;33m'
+BLUE=$CODE'0;34m'
+PURPLE=$CODE'0;35m'
+CYAN=$CODE'0;36m'
+GREY=$CODE'0;37m'
+YELLOW=$CODE'1;33m'
+WHITE=$CODE'1;37m'
+NC=$CODE'0m'
+
 PS3="Choose keymap: "
 maps=("Openauto pro" "Default" "Quit")
 select m in "${maps[@]}"; do
     case $m in 
         "Openauto pro")
-            echo "Using Openauto pro keymap"
+            echo -e "Using ${BLUE}Openauto pro${NC} keymap"
             cp "for car pi"/keys.json src/keys.json
             break
             ;;
         "Default")
-            echo "Using Default keymap"
+            echo -e "Using ${BLUE}Default${NC} keymap"
             cp "default keys"/keys.json src/keys.json
             break
             ;;
         "Quit")
-            echo "Exiting"
+            echo -e "Exiting"
             exit
             break
             ;;
+        *) echo "invalid option $REPLY";;
     esac
 done
 
 # check  if need to create html folder
 if [ -d "./html" ]
 then
-    echo "Directory html exists."
+    echo -e "Directory ${GREEN}html${NC} exists."
 else
-    echo "Creating html folder"
+    echo -e "Creating ${GREEN}html${NC} folder"
     mkdir ./html
 fi
 
@@ -34,9 +50,9 @@ fi
 # check  if need to create html/css folder
 if [ -d "./html/css" ]
 then
-    echo "Directory html/css exists."
+    echo -e "Directory ${GREEN}html/css${NC} exists."
 else
-    echo "Creating html/css folder"
+    echo -e "Creating ${GREEN}html/css${NC} folder"
     mkdir ./html/css
 fi
 
@@ -44,9 +60,9 @@ fi
 # check  if need to create html/js folder
 if [ -d "./html/js" ]
 then
-    echo "Directory html/js exists."
+    echo -e "Directory ${GREEN}html/js${NC} exists."
 else
-    echo "Creating html/js folder"
+    echo -e "Creating ${GREEN}html/js${NC} folder"
     mkdir ./html/js
 fi
 
@@ -54,38 +70,38 @@ fi
 # check  if need to create html/fonts folder
 if [ -d "./html/fonts" ]
 then
-    echo "Directory html/fonts exists."
+    echo -e "Directory ${GREEN}html/fonts${NC} exists."
 else
-    echo "Creating html/fonts folder"
+    echo -e "Creating ${GREEN}html/fonts${NC} folder"
     mkdir ./html/fonts
 fi
         
 
-echo "minifying html5 app files"
+echo -e "${YELLOW}minifying application files${NC}"
 node modules/build.js
 
-echo "File Processing complete"
+echo -e "${YELLOW}File Processing complete${NC}"
 
 PS3="Select OS: "
-maps=("Windows" "ARM" "Quit")
+maps=("Windows" "ARM" "No package")
 select m in "${maps[@]}"; do
     case $m in 
         "Windows")
-            echo "Creating Windows build"
+            echo -e "${YELLOW}Creating Windows build${NC}"
             electron-zip-packager . Configurator --overwrite --asar --platform=win32 --arch=x64 --prune=true --out=release-builds
             break
             ;;
         "ARM")
-            echo "Creating ARMV71 build"
+            echo -e "${YELLOW}Creating ARMV71 build${NC}"
             electron-zip-packager . Configurator --overwrite --asar --platform=linux --arch=armv7l --prune=true --out=release-builds
             break
             ;;
-        "Quit")
-            echo "Exiting"
-            exit
+        "No package")
+            echo -e "${YELLOW}No OS Selected${NC}"
             break
             ;;
+        *) echo "invalid option $REPLY";;
     esac
 done
 
-echo "Application build complete"
+echo -e "${YELLOW}Build complete${NC}"
