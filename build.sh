9e31da2c71023883b6f4a6402b668bf6b6bcd8a5
dough10 <doughten.jimi@gmail.com>
2021-05-20 18:17:05 -0500

    U: formatting again , V: 1.0.5

--- build.sh
+++ build.sh
@@ -14,70 +14,71 @@ WHITE=$CODE'1;37m'
 NC=$CODE'0m'
 
 electronBuild () {
-    electron-zip-packager . Configurator --overwrite --asar --platform=$1 --arch=$2 --prune=true --out=release-builds
+  electron-zip-packager . Configurator --overwrite --asar --platform=$1 --arch=$2 --prune=true --out=release-builds
 }
 
 checkFolder () {
-    if [ -d "./$1" ]
-    then
-        echo -e "Directory ${GREEN}$1${NC} exists."
-    else
-        echo -e "Creating ${GREEN}$1${NC} folder"
-        mkdir ./$1
-    fi 
+  if [ -d "./$1" ]
+  then
+    echo -e "Directory ${GREEN}$1${NC} exists."
+  else
+    echo -e "Creating ${GREEN}$1${NC} folder"
+    mkdir ./$1
+  fi 
 }
 
 # sets to use openauto pro keymap
 useOAPMap () {
-    echo -e "Using ${BLUE}Openauto pro${NC} keymap"
-    cp keymaps/oap.json src/keys.json
+  echo -e "Using ${BLUE}Openauto pro${NC} keymap"
+  cp keymaps/oap.json src/keys.json
 }
 
 # sets keymap as default
 useDefaultMap () {
-    echo -e "Using ${BLUE}Default${NC} keymap"
-    cp keymaps/default.json src/keys.json
+  echo -e "Using ${BLUE}Default${NC} keymap"
+  cp keymaps/default.json src/keys.json
 }
 
 userQuit () {
-    echo -e "Exiting"
-    exit
+  echo -e "Exiting"
+  exit
 }
 
 buildWindows () {
-    echo -e "${YELLOW}Creating Windows build${NC}"
-    electronBuild "win32" "x64"
+  echo -e "${YELLOW}Creating Windows build${NC}"
+  electronBuild "win32" "x64"
 }
 
 buildARM () {
-    echo -e "${YELLOW}Creating ARMV71 build${NC}"
-    electronBuild "linux" "armv7l"
+  echo -e "${YELLOW}Creating ARMV71 build${NC}"
+  electronBuild "linux" "armv7l"
 } 
 
 noBuild () {
-    echo -e "${YELLOW}No OS Selected${NC}"
+  echo -e "${YELLOW}No OS Selected${NC}"
 }
 
 askUser () {
-    PS3="$1: "
-    maps=($2 $3 $4)
-    select m in "${maps[@]}"; do
-        case $m in
-        $2)
-            $5
-            break
-            ;;
-        $3)
-            $6
-            break
-            ;;
-        $4)
-            $7
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-        esac
-    done 
+  PS3="$1: "
+  maps=($2 $3 $4)
+  select m in "${maps[@]}"; do
+    case $m in
+    $2)
+      $5
+      break
+      ;;
+    $3)
+      $6
+      break
+      ;;
+    $4)
+      $7
+      break
+      ;;
+    *) 
+      echo "invalid option $REPLY";;
+    esac
+  done 
 }
 
 askUser "Choose keymap" "Openauto" "Default" "Exit" useOAPMap useDefaultMap userQuit
